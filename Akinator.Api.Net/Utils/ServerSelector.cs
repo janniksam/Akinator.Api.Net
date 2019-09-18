c3c8a1005cc8ce8388c64ef5f1950d16f5f9c283
ahmed605 <34550324+ahmed605@users.noreply.github.com>
2019-09-18 17:03:54 +0200

    Add files via upload

--- Akinator.Api.Net/Utils/ServerSelector.cs
+++ Akinator.Api.Net/Utils/ServerSelector.cs
@@ -29,7 +29,7 @@ namespace Akinator.Api.Net.Utils
                 {
                     Language.Arabic, new[]
                     {
-                        new Server(ServerType.Person, "srv2.akinator.com:9155/ws")
+                        new Server(ServerType.Person, "srv12.akinator.com:9187/ws")
                     }
                 },
                 {
@@ -47,17 +47,9 @@ namespace Akinator.Api.Net.Utils
                     }
                 },
                 {
-                    Language.French, new[]
-                    {
-                        new Server(ServerType.Person, "srv3.akinator.com:9217/ws"),
-                        new Server(ServerType.Object, "srv3.akinator.com:9218/ws"),
-                        new Server(ServerType.Animal, "srv3.akinator.com:9259/ws")
-                    }
-                },
-                {
                     Language.Russian, new[]
                     {
-                        new Server(ServerType.Person, "srv12.akinator.com:9190/ws")
+                        new Server(ServerType.Person, "srv14.akinator.com:9286/ws")
                     }
                 }
             };
