70bb6ab174c7ffd16998bf75bfa45a7e294b058e
Jannik Kowalzik <hugo934753@web.de>
2020-04-11 11:09:30 +0200

    UrlEncode Webutility

--- Akinator.Api.Net/Utils/StringExtensions.cs
+++ Akinator.Api.Net/Utils/StringExtensions.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.IO;
-using System.Text;
+using System.Net;
 
 namespace Akinator.Api.Net.Utils
 {
     public static class StringExtensions
     {
-        static char[] hexChars = "0123456789abcdef".ToCharArray();
-
         public static string UrlEncode(this string input)
         {
             if (input == null)
@@ -15,7 +12,7 @@ namespace Akinator.Api.Net.Utils
                 throw new ArgumentNullException(nameof(input));
             }
 
-            var temp = UrlEncode(input, Encoding.UTF8).ToCharArray();
+            var temp = WebUtility.UrlEncode(input).ToCharArray();
             for (var i = 0; i < temp.Length - 2; i++)
             {
                 if (temp[i] == '%')
@@ -26,134 +23,5 @@ namespace Akinator.Api.Net.Utils
             }
             return new string(temp);
        }
-
-        /// https://github.com/NancyFx/Nancy/blob/de458a9b42db6478e0c2bb8adef0f9fa342a2674/src/Nancy/Helpers/HttpUtility.cs
-        public static string UrlEncode(string s, Encoding Enc)
-        {
-            if (s == null)
-            {
-                return null;
-            }
-
-            if (s == String.Empty)
-            {
-                return string.Empty;
-            }
-
-            bool needEncode = false;
-            int len = s.Length;
-            for (int i = 0; i < len; i++)
-            {
-                char c = s[i];
-                if ((c < '0') || (c < 'A' && c > '9') || (c > 'Z' && c < 'a') || (c > 'z'))
-                {
-                    if (NotEncoded(c))
-                    {
-                        continue;
-                    }
-
-                    needEncode = true;
-                    break;
-                }
-            }
-
-            if (!needEncode)
-            {
-                return s;
-            }
-
-            byte[] bytes = new byte[Enc.GetMaxByteCount(s.Length)];
-            int realLen = Enc.GetBytes(s, 0, s.Length, bytes, 0);
-            return Encoding.ASCII.GetString(UrlEncodeToBytes(bytes, 0, realLen));
-        }
-        internal static bool NotEncoded(char c)
-        {
-            return (c == '!' || c == '(' || c == ')' || c == '*' || c == '-' || c == '.' || c == '_');
-        }
-
-        internal static byte[] UrlEncodeToBytes(byte[] bytes, int offset, int count)
-        {
-            if (bytes == null)
-            {
-                throw new ArgumentNullException(nameof(bytes));
-            }
-
-            int blen = bytes.Length;
-            if (blen == 0)
-            {
-                return new byte[0];
-            }
-
-            if (offset < 0 || offset >= blen)
-                throw new ArgumentOutOfRangeException("offset");
-
-            if (count < 0 || count > blen - offset)
-                throw new ArgumentOutOfRangeException("count");
-
-            MemoryStream result = new MemoryStream(count);
-            int end = offset + count;
-            for (int i = offset; i < end; i++)
-            {
-                UrlEncodeChar((char)bytes[i], result, false);
-            }
-
-            return result.ToArray();
-        }
-
-        internal static void UrlEncodeChar(char c, Stream result, bool isUnicode)
-        {
-            if (c > 255)
-            {
-                int idx;
-                int i = (int)c;
-
-                result.WriteByte((byte)'%');
-                result.WriteByte((byte)'u');
-                idx = i >> 12;
-                result.WriteByte((byte)hexChars[idx]);
-                idx = (i >> 8) & 0x0F;
-                result.WriteByte((byte)hexChars[idx]);
-                idx = (i >> 4) & 0x0F;
-                result.WriteByte((byte)hexChars[idx]);
-                idx = i & 0x0F;
-                result.WriteByte((byte)hexChars[idx]);
-                return;
-            }
-
-            if (c > ' ' && NotEncoded(c))
-            {
-                result.WriteByte((byte)c);
-                return;
-            }
-            if (c == ' ')
-            {
-                result.WriteByte((byte)'+');
-                return;
-            }
-            if ((c < '0') ||
-                (c < 'A' && c > '9') ||
-                (c > 'Z' && c < 'a') ||
-                (c > 'z'))
-            {
-                if (isUnicode && c > 127)
-                {
-                    result.WriteByte((byte)'%');
-                    result.WriteByte((byte)'u');
-                    result.WriteByte((byte)'0');
-                    result.WriteByte((byte)'0');
-                }
-                else
-                    result.WriteByte((byte)'%');
-
-                int idx = ((int)c) >> 4;
-                result.WriteByte((byte)hexChars[idx]);
-                idx = ((int)c) & 0x0F;
-                result.WriteByte((byte)hexChars[idx]);
-            }
-            else
-            {
-                result.WriteByte((byte)c);
-            }
-        }
     }
 }
